eb93a704f9edaf17aba51e0d84464d7a385d798b
HBryan1 <59447508+HBryan1@users.noreply.github.com>
2020-01-21 19:13:03 -0500

    Update contactus.js

--- contactus.js
+++ contactus.js
@@ -1,27 +1,52 @@
-ocument.addEventListener("DOMContentLoaded",init);
-
-
+document.addEventListener("DOMContentLoaded", init);
 
 function init()
 {
-document.getElementById("btn-send").addEventListener("click",validateNames);
- function validateNames()
- {
-    console.log("validateNames is fired!!!");
-    let strAlert="";
-    strAlert=validateFullName(document.getElementById("name").value , document.getElementById("email").value) ;
+  let btn = document.getElementById("submit");
+  let msg = document.getElementById("alertmsg");
+  msg.setAttribute("class", "alert alert-primary");
+  msg.style = "display:none";
+  btn.onclick = submitform;
 
-    function validateFullName(field1,field2)
+  function submitform() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let subject = document.getElementById("subject").value;
+    let message = document.getElementById("message").value;
+
+    if(name.length == 0)
     {
-        console.log("validateNames is fired!!!");
-        if(field1.length == 0 || field2.length == 0 )
-            return "Name or email cannot be empty\n";
-        else
-            return "";
-    
-  } 
-    event.preventDefault();
-    document.getElementById("alertmsg").innerText=strAlert;
-    document.getElementById("alertmsg").style.display="block";
-    document.getElementById("alertmsg").setAttribute("class", "alert alert-success");
+      msg.style = "display:block";
+      msg.innerText = "name box is empty";
+    }
+    else if(name.length != 0)
+    {
+      msg.style = "display:none";
+    }
+
+
+
+
+
+  }
+   function validateNames()
+   {
+      console.log("validateNames is fired!!!");
+      let strAlert="";
+      strAlert=validateFullName(document.getElementById("name").value , document.getElementById("email").value) ;
+
+      function validateFullName(field1,field2)
+      {
+          console.log("validateNames is fired!!!");
+          if(field1.length == 0 || field2.length == 0 )
+              return "Name or email cannot be empty\n";
+          else
+              return "";
+
+    }
+      event.preventDefault();
+      document.getElementById("alertmsg").innerText=strAlert;
+      document.getElementById("alertmsg").style.display="block";
+      document.getElementById("alertmsg").setAttribute("class", "alert alert-success");
+  }
 }
